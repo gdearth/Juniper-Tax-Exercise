a6f900695a99559c8a0ed82e8099802e2ab6813c
Gregg Dearth <gd383608@gmail.com>
2022-02-03 21:22:32 -0500

    Adding project references. Adding Startup.cs for DI.

---
+++ src/Function/Startup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Http;
+
+namespace Function;
+
+public class Startup : FunctionsStartup
+{
+    public override void Configure(IFunctionsHostBuilder builder)
+    {
+        builder.Services.AddLogging(loggingBuilder => loggingBuilder.SetMinimumLevel(LogLevel.Debug));
+        builder.Services.AddHttpClient("TaxJar", client => client.BaseAddress = new Uri("https://api.taxjar.com/v2/"));
+    }
+}
